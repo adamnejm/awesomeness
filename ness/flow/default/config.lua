9ae506f6c4a029e806e5ecf2a355fd9954001c16
Name <adamnejm@gmail.com>
2020-10-11 01:33:56 +0200

    change default layout mode to floating

--- ness/flow/default/config.lua
+++ ness/flow/default/config.lua
@@ -101,6 +101,7 @@ return {
 	
 	-- * List of available layouts to pick from
 	allowed_layouts = {
+		awful.layout.suit.floating,
 		awful.layout.suit.tile,
 		awful.layout.suit.tile.left,
 		awful.layout.suit.tile.bottom,
@@ -113,10 +114,9 @@ return {
 		awful.layout.suit.max.fullscreen,
 		awful.layout.suit.magnifier,
 		awful.layout.suit.corner.nw,
-		awful.layout.suit.corner.ne,
+		-- awful.layout.suit.corner.ne,
 		-- awful.layout.suit.corner.sw,
 		-- awful.layout.suit.corner.se,
-		-- awful.layout.suit.floating,
 	},
 	
 	
